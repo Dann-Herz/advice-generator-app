e0413d8fe8128c6a5b5042ccc3d92fc34979d620
Dann Herz <danielhdzmzin@gmail.com>
2022-03-01 13:24:05 -0600

    I finished the presentation of this proyect, it's use an API of differents advices, so you can click the dice and get a new advice! for now it's all

---
+++ index.js
@@ -0,0 +1,23 @@
+const API = 'https://api.adviceslip.com/advice';
+const number = document.querySelector(".number");
+const advice = document.querySelector(".texto");
+const dice = document.querySelector(".container_dice");
+
+dice.addEventListener("click", fetchear);
+
+async function fetchear() {
+  try {
+    let response = await fetch(API).then(response => response.json());
+    number.innerHTML = `Advice #${response.slip.id}`
+    advice.innerHTML = response.slip.advice;
+    console.log(response.slip.advice)
+  } catch (error) {
+    console.log(error.message)
+  }
+
+}
+fetchear();
+
+// fetch(API)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
